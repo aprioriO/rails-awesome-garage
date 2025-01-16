1fd490f5289503f5a9abc4cdd0b2bb2297b20171
Olena Hulakova <hulakova.olena@gmail.com>
2025-01-16 14:41:30 +0000

    Relations and dependencies and validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :owner
+
+  has_many :favorites, dependent: :destroy
+  has_many :rewies
+
+  validates :brand, :model, :fuel, presence :true
+  validates :year, presence :true, numericality: {only_integer: true}
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,5 @@
 class Owner < ApplicationRecord
+  has_many :cars
+
+  validates :nickname, uniqueness: true, presence :true
 end
- 
